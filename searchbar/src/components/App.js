a4321ae172fd92d8b9df6cac51ce704cef8777f7
unknown <kiran.mohare@pearson.com>
2020-07-21 11:58:08 -0600

    first commit  Please enter the message for your changes. Lines starting

---
+++ searchbar/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// class App extends React.component{
+// 	render(){
+// 		return (
+// 			<div> App </div>
+// 		);
+// 	}
+// }
+
+const App = () => {
+	return <div>App</div>;
+};
+
+export default App;
